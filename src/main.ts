861ac9d6895e21449190edecf95329796c3fd94b
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-09-27 09:55:10 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { importSvg, svgList } from 'unplugin-svg-transformer/icons'
+
+const svg: SvgType = 'vite'
+
+importSvg(svg).then((res) => {
+  document.getElementById('vite')!.innerHTML = res
+})
+
+importSvg('social/twitter').then((res) => {
+  document.getElementById('twitter')!.innerHTML = res
+})
+
+let list: string[] = []
+let ul = document.getElementById('list')!
+
+for (const item of Object.entries(svgList)) {
+  let name = item[0]
+  list.push(name)
+  ul.innerHTML += `<li>${name}</li>`
+}
